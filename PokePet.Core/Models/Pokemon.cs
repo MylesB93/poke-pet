6c3a7970c4531a6eff74891e22348403a63b793a
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-01-28 18:20:45 +0000

    Add properties to Pokemon class

--- PokePet.Core/Models/Pokemon.cs
+++ PokePet.Core/Models/Pokemon.cs
@@ -1,7 +1,15 @@
-﻿namespace PokePet.Core.Models
+﻿using System.Text.Json.Serialization;
+
+namespace PokePet.Core.Models
 {
 	// All the code in this file is included in all platforms.
 	public class Pokemon
 	{
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+		[JsonPropertyName("is_baby")]
+		public bool IsBaby { get; set; }
 	}
 }
