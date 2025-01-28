770a0c6c8bc0684fc99423ce68619216c5548e1b
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-01-28 18:50:33 +0000

    Install GraphQL package and add pokeservice

---
+++ PokePet.Core/PokeService.cs
@@ -0,0 +1,19 @@
+﻿using PokePet.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokePet.Core
+{
+	public class PokeService
+	{
+		public Pokemon GetPokemon(string name)
+		{
+
+
+			return new Pokemon();
+		}
+	}
+}
