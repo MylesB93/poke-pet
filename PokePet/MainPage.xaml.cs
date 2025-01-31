47b6a160c48c1abd803389bb5b861b27b5863c10
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-01-31 23:35:17 +0000

    Remove button

--- PokePet/MainPage.xaml.cs
+++ PokePet/MainPage.xaml.cs
@@ -14,18 +14,6 @@ namespace PokePet
 			_pokeService = pokeService;
 			InitializeComponent();
         }
-
-        private async void OnCounterClicked(object sender, EventArgs e)
-        {
-			count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
