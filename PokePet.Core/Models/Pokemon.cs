c1d0c4eea1e167065db219f283efaf018487c143
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-02-03 19:38:31 +0000

    Add pokemon ID primary key

--- PokePet.Core/Models/Pokemon.cs
+++ PokePet.Core/Models/Pokemon.cs
@@ -6,8 +6,10 @@ namespace PokePet.Core.Models
 	[Table("pokemon")]
 	public class Pokemon
 	{
+		[PrimaryKey, AutoIncrement]
+		public int PokemonId { get; set; } // TODO: figure out how to make this auto increment (currently being initialised as 0)
 		[JsonPropertyName("id")]
-		public int PokemonId { get; set; }
+		public int Id { get; set; }
 		[JsonPropertyName("name")]
 		public string? Name { get; set; }
 		[JsonPropertyName("is_baby")]
