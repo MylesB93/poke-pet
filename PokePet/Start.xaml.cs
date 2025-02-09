f26634c20af6033143bf7b279b6b5b0939d5d239
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-02-09 21:02:02 +0000

    Add start page

---
+++ PokePet/Start.xaml.cs
@@ -0,0 +1,14 @@
+namespace PokePet;
+
+public partial class Start : ContentPage
+{
+	public Start()
+	{
+		InitializeComponent();
+	}
+
+	public async void OnSearchButtonClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///Search");
+	}
+}
