8dfab8d38b08b543aea396c11948070ce08f7d8b
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-04-08 22:53:11 +0100

    WIP: Add enum description converter to convert boredom values

--- PokePet.Core/Enums/Boredom.cs
+++ PokePet.Core/Enums/Boredom.cs
@@ -1,10 +1,19 @@
-﻿namespace PokePet.Core.Enums
+﻿using System.ComponentModel;
+
+namespace PokePet.Core.Enums
 {
 	public enum Boredom
 	{
+		[Description("Not Bored")]
 		NotBored,
+
+		[Description("Slightly Bored")]
 		SlightlyBored,
+
+		[Description("Bored")]
 		Bored,
+
+		[Description("Really Bored")]
 		ReallyBored
 	}
 }
---
+++ PokePet.Core/Enums/EnumDescriptionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace PokePet.Core.Enums
+{
+	public class EnumDescriptionConverter : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is Enum enumVal)
+			{
+				var field = enumVal.GetType().GetField(enumVal.ToString());
+				var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+				return attribute?.Description ?? enumVal.ToString();
+			}
+
+			return value?.ToString() ?? string.Empty;
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
